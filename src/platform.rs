3efeb78b018fe93f0ca57a0892666bf83ed22461
Andy Goetz <andy@andygoetz.org>
2021-12-23 16:00:35 -0800

    Use opener for platform specific launching code
    the current implementation of jolly detects what platform we are on and
    uses xdg-open or explorer to open the file as appropriate.
    
    However, there are some corner cases on Windows platforms in terms of
    how the explorer.exe handles shell arguments, which means that urls
    with query strings were not being handled properly. The solution is to start
    using a standalone crate `opener` to handle opening of the files. This works
    for all 3 platforms and allows simplifying the url logic.

--- src/platform.rs
+++ src/platform.rs
@@ -1,59 +1,9 @@
-use std::fmt;
+use opener;
 use std::path::Path;
-use std::process::{Command, Stdio};
 
-#[derive(Debug)]
-pub enum Error {
-    IOError(std::io::Error),
-    OpenError(String),
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match self {
-            Error::IOError(e) => e.fmt(f),
-            Error::OpenError(p) => {
-                write!(f, "Could not open path: ")?;
-                p.fmt(f)
-            }
-        }
-    }
-}
-
-struct PlatformOpts {
-    open_cmd: &'static str,
-    ignore_errors: bool,
-}
-
-#[cfg(target_os = "macos")]
-const OPTS: PlatformOpts = PlatformOpts {
-    open_cmd: "open",
-    ignore_errors: false,
-};
-
-#[cfg(target_os = "windows")]
-const OPTS: PlatformOpts = PlatformOpts {
-    open_cmd: "explorer.exe",
-    ignore_errors: true,
-};
-
-#[cfg(target_os = "linux")]
-const OPTS: PlatformOpts = PlatformOpts {
-    open_cmd: "xdg-open",
-    ignore_errors: false,
-};
+pub type Error = opener::OpenError;
 
 pub fn open_file<P: AsRef<Path>>(path: P) -> Result<(), Error> {
-    let result = Command::new(OPTS.open_cmd)
-        .arg(path.as_ref().as_os_str())
-        .stdin(Stdio::null())
-        .stdout(Stdio::null())
-        .stderr(Stdio::null())
-        .status()
-        .map_err(Error::IOError)?;
-    if result.success() || OPTS.ignore_errors {
-        Ok(())
-    } else {
-        Err(Error::OpenError(path.as_ref().display().to_string()))
-    }
+    println!("{}", path.as_ref().display());
+    opener::open(path.as_ref().as_os_str())
 }
