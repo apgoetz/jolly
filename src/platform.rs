f18b1a49f1c652de7f4c21cfdf9af5b4af9b9da9
Andy Goetz <andy@andygoetz.org>
2021-12-23 20:51:08 -0800

    Make Opener errors more user friendly.
    The default opener error formatter uses a multine display that is not
    suitable for display in jolly. So we customize the error message to fit
    in a single line.

--- src/platform.rs
+++ src/platform.rs
@@ -1,8 +1,22 @@
 use opener;
+use std::error;
+use std::fmt;
 use std::path::Path;
 
-pub type Error = opener::OpenError;
+#[derive(Debug)]
+pub struct Error(opener::OpenError);
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self.0 {
+            opener::OpenError::ExitStatus { stderr: e, .. } => f.write_str(e),
+            _ => self.0.fmt(f),
+        }
+    }
+}
+
+impl error::Error for Error {}
 
 pub fn open_file<P: AsRef<Path>>(path: P) -> Result<(), Error> {
-    opener::open(path.as_ref().as_os_str())
+    opener::open(path.as_ref().as_os_str()).map_err(Error)
 }
