edfec315b7d5553861fc254baff468ad30638d33
Andy Goetz <andy@andygoetz.org>
2021-12-23 19:50:04 -0800

    removed accidental println!

--- src/platform.rs
+++ src/platform.rs
@@ -4,6 +4,5 @@ use std::path::Path;
 pub type Error = opener::OpenError;
 
 pub fn open_file<P: AsRef<Path>>(path: P) -> Result<(), Error> {
-    println!("{}", path.as_ref().display());
     opener::open(path.as_ref().as_os_str())
 }
